ca7be6aa42d87063479366ced8c61e912a7d8bc4
Fayeed Pawaskar <fayeed@live.com>
2024-02-01 20:54:54 +0530

    Updated to work with continents

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{collections::HashMap, env};
+use std::{collections::HashMap, env, net::{IpAddr, SocketAddr}};
+use maxminddb::geoip2::{self, enterprise::Continent};
+use std::str::FromStr;
 use serde_json::{json, to_string};
 use ring::digest;
 use bytes::Bytes;
@@ -11,6 +13,11 @@ use lazy_static::lazy_static;
 use did_key::{CoreSign, PatchedKeyPair};
 
 use hex::decode;
+
+const SG_PROXY_SERVER: &str = "https://proxy-sg.ad4m.dev";
+const US_PROXY_SERVER: &str = "https://proxy-us.ad4m.dev";
+const DE_PROXY_SERVER: &str = "https://proxy-de.ad4m.dev";
+
 fn build_message_raw(data: serde_json::Value) -> Vec<u8> {
     let payload_string = to_string(&data).expect("Error serializing JSON");
 
@@ -38,10 +45,10 @@ pub struct Proxy {
 }
 
 lazy_static! {
-    static ref API_TOKEN: String = env::var("API_TOKEN").unwrap_or_else(|_| panic!("API_TOKEN env variable is missing"));
-    static ref ACCOUNT_ID: String = env::var("ACCOUNT_ID").unwrap_or_else(|_| panic!("ACCOUNT_ID env variable is missing"));
-    static ref RAND_KV: String = env::var("RAND_KV").unwrap_or_else(|_| panic!("RAND_KV env variable is missing"));
-    static ref CREDENTIALS_KV: String = env::var("CREDENTIALS_KV").unwrap_or_else(|_| panic!("CREDENTIALS_KV env variable is missing"));
+    static ref API_TOKEN: String = env::var("API_TOKEN").unwrap_or_else(|_| panic!("No API_TOKEN set"));
+    static ref ACCOUNT_ID: String = env::var("ACCOUNT_ID").unwrap_or_else(|_| panic!("No ACCOUNT_ID set"));
+    static ref RAND_KV: String = env::var("RAND_KV").unwrap_or_else(|_| panic!("No RAND_KV set"));
+    static ref CREDENTIALS_KV: String = env::var("CREDENTIALS_KV").unwrap_or_else(|_| panic!("No CREDENTIALS_KV set"));
 }
 
 async fn store_value(store_name: &str, key: &str, value: &str) -> Result<(), String> {
@@ -85,6 +92,36 @@ async fn get_value(store_name: &str, key: &str) -> Result<String, String> {
     }
 }
 
+async fn get_continent(ip: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let reader = maxminddb::Reader::open_readfile("src/GeoLite2-City.mmdb").unwrap();
+    let ip: IpAddr = FromStr::from_str(ip).unwrap();
+    let city: geoip2::City = reader.lookup(ip).unwrap_or(geoip2::City {
+        continent: Some(Continent {
+            code: Some("AS"),
+            geoname_id: None,
+            names: None,
+        }),
+        country: None,
+        location: None,
+        postal: None,
+        registered_country: None,
+        represented_country: None,
+        traits: None,
+        city: None,
+        subdivisions: None,
+    });
+
+    match city.continent {
+        Some(continent) => {
+            match continent.code {
+                Some(code) => Ok(code.to_string()),
+                None => Ok(String::from("AS")),
+            }
+        },
+        None => Ok(String::from("AS")),
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let login_route = warp::path!("login")
@@ -97,6 +134,7 @@ async fn main() {
 
     let default_route = warp::path::full()
         .and(warp::query::<HashMap<String, String>>())
+        .and(warp::addr::remote())
         .and_then(default_handler);
 
     let routes = login_route.or(login_verify_route).or(default_route);
@@ -104,11 +142,31 @@ async fn main() {
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-async fn default_handler(path: warp::path::FullPath, query_params: HashMap<String, String>) -> Result<impl Reply, Rejection> {
+async fn default_handler(path: warp::path::FullPath, query_params: HashMap<String, String>, remote_addr: Option<SocketAddr>,) -> Result<impl Reply, Rejection> {
+    let ip = match remote_addr {
+        Some(addr) => addr.ip().to_string(),
+        None => return Ok(warp::reply::json(&"error".to_string())),
+    };
+
+    let ip_addr = remote_addr.unwrap().ip().to_string();
+
+    let continent_result = get_continent(ip_addr.as_str()).await.unwrap();
+
+    let mut continent_map: HashMap<&str, &str> = HashMap::new();
+    continent_map.insert("AF", DE_PROXY_SERVER);
+    continent_map.insert("AN", US_PROXY_SERVER);
+    continent_map.insert("AS", SG_PROXY_SERVER);
+    continent_map.insert("EU", DE_PROXY_SERVER);
+    continent_map.insert("NA", US_PROXY_SERVER);
+    continent_map.insert("OC", SG_PROXY_SERVER);
+    continent_map.insert("SA", US_PROXY_SERVER);
+
+    let url = continent_map.get(continent_result.as_str()).unwrap().to_string();
+
     let path = path.as_str();
     let query = serde_urlencoded::to_string(&query_params).unwrap_or_default();
 
-    let dest_path = format!("https://proxy-sg.ad4m.dev{}?{}", path.to_lowercase(), query);
+    let dest_path = format!("{}{}?{}", url,path.to_lowercase(), query);
 
     let client = Client::new();
 
@@ -173,5 +231,17 @@ async fn login_verify_handler(query_params: HashMap<String, String>) -> Result<i
         }
     } else {
         Ok(warp::reply::with_status("Missing parameters".to_string(), warp::http::StatusCode::OK))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_continent() {
+        let ip = "0.0.0.0"; 
+        let continent = get_continent(ip).await.unwrap();
+        assert_eq!(continent, "NA");
     }
 }
